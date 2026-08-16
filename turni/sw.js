545f0e778387e9eae85a2e6f98641281e0e7c44a
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-08-17 00:55:02 +0200

    Bump app cache for request workflow

--- turni/sw.js
+++ turni/sw.js
@@ -1,4 +1,4 @@
-const CACHE='torre-maura-turni-v12';
+const CACHE='torre-maura-turni-v13';
 const ASSETS=['./','index.html','styles.css','common.js','app.js','admin.html','admin.js','requests.html','requests.js','inbox.html','inbox.js','avvisi.html','avvisi.js','manifest.webmanifest','icon.svg','schedule.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
