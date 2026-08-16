8f9a85499821ab454479a12ba6de32a9ca8af3d5
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-08-16 16:06:18 +0200

    Forza nuova cache restyling Eurospin

--- turni/sw.js
+++ turni/sw.js
@@ -1,4 +1,4 @@
-const CACHE='torre-maura-turni-v6';
+const CACHE='torre-maura-turni-v7';
 const ASSETS=['./','index.html','styles.css','common.js','app.js','admin.html','admin.js','requests.html','requests.js','inbox.html','inbox.js','avvisi.html','avvisi.js','manifest.webmanifest','icon.svg','schedule.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
