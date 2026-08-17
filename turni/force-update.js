a7d54d9e25f6520cae759dd8686eb3f0612fa493
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-08-17 13:46:21 +0200

    Force PWA update v54

--- turni/force-update.js
+++ turni/force-update.js
@@ -1,9 +1,9 @@
 (()=>{
  if(!('serviceWorker' in navigator))return;
- const KEY='tm_force_sw_v53';
+ const KEY='tm_force_sw_v54';
  window.addEventListener('load',async()=>{
   try{
-   const reg=await navigator.serviceWorker.register('sw.js?v=53',{scope:'./'});
+   const reg=await navigator.serviceWorker.register('sw.js?v=54',{scope:'./'});
    await reg.update();
    if(!localStorage.getItem(KEY)){
     localStorage.setItem(KEY,'1');
