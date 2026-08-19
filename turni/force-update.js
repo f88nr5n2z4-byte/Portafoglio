23ec6b218eee92d6150bf2bbef9dca2801a5ab8a
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-08-19 22:43:41 +0200

    Forza runtime v140

--- turni/force-update.js
+++ turni/force-update.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='v139';
+const VERSION='v140';
 async function cleanup(){
  try{
   if('caches' in window){const keys=await caches.keys();await Promise.all(keys.map(k=>caches.delete(k)))}
