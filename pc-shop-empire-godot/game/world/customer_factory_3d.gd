370091247260c59ba890cda9636462a92b273cde
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-08-22 16:16:38 +0200

    M0 NPC: add four-look ambient customer set routes

--- pc-shop-empire-godot/game/world/customer_factory_3d.gd
+++ pc-shop-empire-godot/game/world/customer_factory_3d.gd
@@ -8,3 +8,10 @@ static func spawn_customer(parent:Node3D,customer_script:Script,visual_script:Sc
 	parent.add_child(actor)
 	actor.set_route(route)
 	return actor
+
+static func spawn_ambient_set(parent:Node3D,customer_script:Script,visual_script:Script)->Array[CharacterBody3D]:
+	var actors:Array[CharacterBody3D]=[]
+	actors.append(spawn_customer(parent,customer_script,visual_script,"Customer_Gamer",Vector3(-4.4,0.02,6.7),[Vector3(-4.4,0.02,4.9),Vector3(-3.0,0.02,3.2),Vector3(-5.8,0.02,-1.7)],Color("#6a3fc0"),Color("#1d2631"),Color("#c78f6e"),"gamer"))
+	actors.append(spawn_customer(parent,customer_script,visual_script,"Customer_CasualWoman",Vector3(4.5,0.02,6.6),[Vector3(4.1,0.02,4.7),Vector3(2.8,0.02,3.4),Vector3(6.15,0.02,-3.7)],Color("#b54e78"),Color("#25303b"),Color("#d3a17e"),"female_casual"))
+	actors.append(spawn_customer(parent,customer_script,visual_script,"Customer_Professional",Vector3(7.2,0.02,6.0),[Vector3(6.0,0.02,4.7),Vector3(5.5,0.02,2.4),Vector3(5.7,0.02,0.2)],Color("#344b63"),Color("#202832"),Color("#b98465"),"professional"))
+	return actors
