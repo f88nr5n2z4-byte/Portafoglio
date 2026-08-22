78e2b84007c0041a147adb8da1d3f31f48c5fff2
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-08-22 06:43:07 +0200

    Add inheritance-chain parse diagnostic for final beta gate

---
+++ pc-shop-empire-godot/qa_parse_chain.gd
@@ -0,0 +1,28 @@
+extends SceneTree
+
+func _init() -> void:
+	var paths := [
+		"res://beta.gd",
+		"res://beta_runtime.gd",
+		"res://beta_polish.gd",
+		"res://beta_release.gd",
+		"res://beta_final.gd",
+		"res://beta_commercial.gd",
+		"res://beta_ship.gd",
+		"res://beta_hotfix.gd",
+		"res://qa_game.gd"
+	]
+	var failed := false
+	for path in paths:
+		print("PARSE CHECK START: ", path)
+		var script: Script = load(path)
+		if script == null or not script.can_instantiate():
+			printerr("PARSE CHECK FAIL: ", path)
+			failed = true
+			break
+		print("PARSE CHECK PASS: ", path)
+	if failed:
+		quit(1)
+	else:
+		print("PARSE CHAIN: ALL SCRIPTS INSTANTIABLE")
+		quit(0)
