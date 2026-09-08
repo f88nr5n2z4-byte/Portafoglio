e539785edfcaae979b2fd6f32ca4932f9fe2a311
f88nr5n2z4-byte <f88nr5n2z4@privaterelay.appleid.com>
2026-09-08 20:28:26 +0200

    Bump unified Turni cache generation

--- turni-v2/sw.js
+++ turni-v2/sw.js
@@ -1,4 +1,4 @@
-const CACHE='turni-current-20260908-v1';
+const CACHE='turni-current-20260908-v2';
 const ASSETS=['./','./index.html','./app-current.css','./app-current.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('turni-')&&k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
